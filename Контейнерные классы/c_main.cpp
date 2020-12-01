2cbf4dcbf371941b8806f991068c60346eb5c177
twincaret <twincaret@gmail.com>
2020-12-01 20:32:18 +0300

    upd

--- Контейнерные классы/c_main.cpp
+++ Контейнерные классы/c_main.cpp
@@ -1,7 +1,7 @@
 #include "cont.h"
 #include "cont.cpp"
-// #include <algorithm>
-// #include <vector>
+#include <algorithm>  // for lib 
+#include <vector>        // func
 
 int main()
 {
@@ -13,49 +13,45 @@ int main()
 	ini(a); // array 'a' init
 	std::cout << "\nNew generated array: ";
 	print(a);
-	// clone array to vector to work with lib func
-    /*  
+	// clone array to vector to work with lib func   
 	std::vector<int> v(n);
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < n; ++i)
 	{
 		v[i] = a[i];
 	}
-	for(auto i : v)
-	{
-		std::cout << i << " ";
-	}
-	*/
+    /* for( auto i : v ) { std::cout << i << " "; } */
 	std::cout << std::endl;
 	std::cout << "Array length is: " << a.getLength() << " elements\n" << std::endl;
 
 	std::uint16_t startPoint = clock();
 	quickSort(a, 0, n - 1); // custom func
-	/*
+	std::uint16_t finalPoint1 = clock() - startPoint;
+	startPoint = clock();
     std::sort(v.begin(), v.end()); // lib func
-	*/
-	std::uint16_t finalPoint = clock() - startPoint;
+	std::uint16_t finalPoint2 = clock() - startPoint;
 	std::cout << "Sorted array: ";
-	print(a);
 	// print sorted 
-	/*
-	for(auto i : v)
-	{
-		std::cout << i << " ";
-	}
-	*/
+	print(a);
+
 	std::cout << std::endl;
-	std::cout << "Sorting time: " << finalPoint << " ms.\n";
+	std::cout << "Sorting time: custom func " << finalPoint1 << " ms. vs lib func "<< finalPoint2 << " ms.\n";
 	std::cout << std::endl;
 
 	std::cout << "Enter the value you wish to find in the array: ";
 	std::cin >> n;
 	std::cout << "Value ";
-
+    /* custom search func */
+	startPoint = clock();
 	(binsearch(a, n) == -1? std::cout << "not found" << std::endl : std::cout << n << " has index " << binsearch(a, n) << std::endl);
-	/*
+	finalPoint1 = clock() - startPoint;
+	//std::cout << "Value ";
+	/* lib search algorithm */
+	startPoint = clock();
     auto x = std::find(v.begin(), v.end(), n);
-	(x == v.end()? std::cout << "not found" << std::endl : std::cout << n << " has index " << x - v.begin() << std::endl);
-	*/
+	(x == v.end()? std::cout/*<< "not found" */<< std::endl : std::cout/* << n << " has index " << x - v.begin() */<< std::endl);
+	finalPoint2 = clock() - startPoint;
+	std::cout << "Search time: custom func " << finalPoint1 << " ms. vs lib func "<< finalPoint2 << " ms.\n";
+
 	std::cout << "\nEnter new length for the array: ";
 	std::cin >> n;
 	try
@@ -83,4 +79,4 @@ int main()
 	return 0;
 
 }
-// sort && search algorithm added
+
---
+++ Лямбда-контейнеры/ТЕМА: Стандартные контейнеры/emp.cpp
@@ -0,0 +1,46 @@
+#include <iostream>     
+#include <string>
+ 
+ using namespace std;
+ 
+class Employee 
+{
+    string name;
+    long numb;
+    
+public:
+    void getdata()
+    {
+        cout << "\nName ";  getline (cin, name);
+        cout << "Number ";  (cin >> numb).get();
+        cout << endl;
+    }
+    void putdata()const
+    {
+        cout << "\nName " << name << "\nNumber " << numb;
+    }
+};
+    
+int main()
+{
+    Employee e [100];
+    char x;
+    int n = 0;
+    do
+    {
+        cout << "Employee data: ";
+        e [n++].getdata();
+        cout << "Continue ? (y/n): "; (cin >> x).get();
+        cout << endl;
+    }
+    while( x != 'n' && x != 'N');
+ 
+    for( int i = 0; i < n; ++i )
+    {
+        cout << "\nEmployee " << i + 1;  e[i].putdata();
+        cout << endl;
+    }
+    cout << endl;
+ 
+    return 0;
+}
